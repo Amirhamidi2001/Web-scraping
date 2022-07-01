81b61ff5ccafc515cf50c291a4a3cbca01929268
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-07-01 09:37:47 +0430

    Update source code

--- bay_car.py
+++ bay_car.py
@@ -11,29 +11,29 @@ import mysql.connector
 from bs4 import BeautifulSoup
 from sklearn import preprocessing
 
-cnx = mysql.connector.connect(user = 'root',
-                              host = 'localhost',
-                              password = 'root',
-                              database = 'car')
+cnx = mysql.connector.connect(user='root',
+                              host='localhost',
+                              password='root',
+                              database='car')
 
 url = 'https://www.cars.com/shopping/results/?page='
 for page in range(1, 100):
     r = requests.get(url + str(page))
-    
+
     soup = BeautifulSoup(r.text, 'html.parser')
-    ads = soup.find_all('div', attrs= {'class' : 'vehicle-details'})
-    
+    ads = soup.find_all('div', attrs={'class': 'vehicle-details'})
+
     for ad in ads:
-        car_name = (ad.find('a', attrs = {'class' : 'vehicle-card-link'}).text[6:])
-        car_year = (ad.find('a', attrs = {'class' : 'vehicle-card-link'}).text[1:5])
-        car_kms = (ad.find('div', attrs = {'class' : 'mileage'}).text)
-        car_price = (ad.find('div', attrs = {'class' : 'price-section'}).text)
-        
+        car_name = (ad.find('a', attrs={'class': 'vehicle-card-link'}).text[6:])
+        car_year = (ad.find('a', attrs={'class': 'vehicle-card-link'}).text[1:5])
+        car_kms = (ad.find('div', attrs={'class': 'mileage'}).text)
+        car_price = (ad.find('div', attrs={'class': 'price-section'}).text)
+
         cursor = cnx.cursor()
-        cursor.execute('INSERT INTO ads VALUES (\'%s\',\'%s\',\'%s\',\'%s\')'%(car_name,
-                                                                               car_year,
-                                                                               car_kms,
-                                                                               car_price,))
+        cursor.execute('INSERT INTO ads VALUES (\'%s\',\'%s\',\'%s\',\'%s\')' % (car_name,
+                                                                                 car_year,
+                                                                                 car_kms,
+                                                                                 car_price,))
         cnx.commit()
 
 cursor = cnx.cursor()
@@ -50,4 +50,3 @@ for car in cars:
     le = le.fit([title, year, kms, price])
 
 cnx.close()
-
